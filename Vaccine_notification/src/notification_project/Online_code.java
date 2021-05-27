3c8d90fed9bf052c5c0e9694a81e1e8e2a79f66b
mateek <mateek@INW1PF0TFNJJ.informatica.com>
2021-05-27 19:34:17 +0530

    After formting

--- Vaccine_notification/src/notification_project/Online_code.java
+++ Vaccine_notification/src/notification_project/Online_code.java
@@ -1,5 +1,7 @@
 package notification_project;
+import java.awt.Desktop;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -20,21 +22,28 @@ public class Online_code {
 	
 	private static String GET_URL_COVISHIELD = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?";
 	
+	private static String  SESSION = "sessions";
+	
+	private static String NOTIFICATION_FILE_PATH = "C:\\Users\\mateek\\Desktop\\Slot_Notification.html";
+	
 	private static Boolean END = false;
+	
+	private static int AGE = 45;
+	
+	private static int PIN = 471001;
+	
+	private static String TODAY_DATE = "28-05-2021";
+	
+	private static String TYPE = "COVAXIN"; //COVISHIELD
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int pin = 471001;
-		String date = "22-05-2021";
-		String type = "COVAXIN"; //(COVAXIN\COVISHIELD)
-		GET_URL_COVAXIN += "pincode="+pin	//+"110001"+
-				+"&date="+date	//+"31-03-2021
-				+"&vaccine="+type;//+"COVISHIELD"
-				
-		GET_URL_COVISHIELD += "pincode="+pin	//+"110001"+
-				+"&date="+date	//+"31-03-2021
-				+"&vaccine="+"COVISHIELD";//+"COVISHIELD"
-				
+		
+		//(COVAXIN\COVISHIELD)
+		GET_URL_COVAXIN += "pincode="+PIN	//+"110001"+
+				+"&date="+TODAY_DATE	//+"31-03-2021
+				+"&vaccine="+TYPE;//+"COVISHIELD"
+		System.out.println("start checking for vaccine at pin " + PIN + "| date " + TODAY_DATE);
 		while(true) {			
 			checkStatus();
 			if(END)
@@ -54,17 +63,57 @@ public class Online_code {
 
 		Date date = new Date();
 		System.out.println("Checking at :"+date.toString());
-		String response1 = "";
-		String response2 = "";
+		String response = "";
 		try {
-			response1 = sendGET(GET_URL_COVAXIN);
-			response2 = sendGET(GET_URL_COVISHIELD);
+			response = sendGET(GET_URL_COVAXIN);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		//System.out.println(response.toString());
-		formatResponse(response1,response2);
+		ArrayList<Map>  res = formatResponse(response);
+
+		res = checkAvailability(res);
+		if(res.size()>0)
+			notifyUser(res);
+		//System.out.println(emp.toString());
+	}
+
+	private static void notifyUser(ArrayList<Map> res) {
+		// TODO Auto-generated method stub
+		
+		File htmlFile = new File(NOTIFICATION_FILE_PATH);
+		
+		try {
+			Desktop.getDesktop().browse(htmlFile.toURI());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	private static ArrayList<Map> checkAvailability(ArrayList<Map> res) {
+		// TODO Auto-generated method stub
+		ArrayList<Map> availableSlot = new ArrayList<Map>();
+		for (Map center : res) {
+			int age = (int) center.get("min_age_limit");
+			if(age == AGE) {
+				int dose1 = (int) center.get("available_capacity_dose1");
+				if(dose1>0) {
+					System.out.println("available for 45+ :" + center);
+					availableSlot.add(center);
+					//END = true;
+				}
+			}else if( age == 18) {
+				int dose1 = (int) center.get("available_capacity_dose1");
+				if(dose1>0) {
+					System.out.println("available for 18+ :" +center);
+					availableSlot.add(center);
+				}
+			}
+		}
+		return availableSlot;
 	}
 
 	private static String sendGET(String URL) throws IOException {
@@ -88,29 +137,18 @@ public class Online_code {
 		return response.toString();
 	}
 
-	private static void formatResponse(String response1,String response2) {
+	private static ArrayList<Map> formatResponse(String response) {
 		// TODO Auto-generated method stub
 		ObjectMapper mapper = new ObjectMapper();
 	      try {
-			Map res1 = mapper.readValue(response1, HashMap.class);
-			Map res2 = mapper.readValue(response2, HashMap.class);
+			Map res1 = mapper.readValue(response, HashMap.class);
 			
-			ArrayList<Map> covidCenters = (ArrayList<Map>) res1.get("sessions");
-			covidCenters.addAll(((ArrayList<Map>) res2.get("sessions")));
-			for (Map center : covidCenters) {
-				int age = (int) center.get("min_age_limit");
-				if(age == 45) {
-					int dose1 = (int) center.get("available_capacity_dose1");
-					if(dose1>0) {
-						System.out.println("available");
-						END = true;
-					}
-				}
-			}
-			//System.out.println(emp.toString());
+			 return (ArrayList<Map>) res1.get(SESSION);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	   
+		}
+		return null;	   
 	}
 }
